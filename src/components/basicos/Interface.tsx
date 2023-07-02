146f7cb47453507a41f2ce313ca57fc72d0daeee
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-02 18:47:15 -0300

    Updates

--- src/components/basicos/Interface.tsx
+++ src/components/basicos/Interface.tsx
@@ -1,7 +1,34 @@
 export default function Interface() {
     return (
 
-        <h1>Interface</h1>
+        <div className="interface">
+
+            <header>
+                <h1 className="brilhante">META CASINO</h1>
+                <div className="pyramid-loader">
+                    <div className="wrapper">
+                    <span className="side side1"></span>
+                    <span className="side side2"></span>
+                    <span className="side side3"></span>
+                    <span className="side side4"></span>
+                    <span className="shadow"></span>
+                    </div>  
+                </div>
+            </header>
+
+            <button className="btn start-btn" type="button">
+                <strong>START</strong>
+                <div id="container-stars">
+                <div id="stars"></div>
+                </div>
+            
+                <div id="glow">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                </div>
+            </button>
+
+        </div>
         
     )
 }
