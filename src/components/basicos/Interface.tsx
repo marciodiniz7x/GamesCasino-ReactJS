b9209e32e2bb27ca357dc566f159d6a01e31b9c2
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-02 19:21:24 -0300

    Update Interface.tsx

--- src/components/basicos/Interface.tsx
+++ src/components/basicos/Interface.tsx
@@ -1,34 +1,38 @@
+import React from 'react';
+
 export default function Interface() {
-    return (
 
-        <div className="interface">
+  const handleClick = () => {
+    alert('Aplicação ainda em Desenvolvimento!');
+  };
 
-            <header>
-                <h1 className="brilhante">META CASINO</h1>
-                <div className="pyramid-loader">
-                    <div className="wrapper">
-                    <span className="side side1"></span>
-                    <span className="side side2"></span>
-                    <span className="side side3"></span>
-                    <span className="side side4"></span>
-                    <span className="shadow"></span>
-                    </div>  
-                </div>
-            </header>
-
-            <button className="btn start-btn" type="button">
-                <strong>START</strong>
-                <div id="container-stars">
-                <div id="stars"></div>
-                </div>
-            
-                <div id="glow">
-                <div className="circle"></div>
-                <div className="circle"></div>
-                </div>
-            </button>
-
+  return (
+    <div className="interface">
+      <header>
+        <h1 className="brilhante">META CASINO</h1>
+        <div className="pyramid-loader">
+          <div className="wrapper">
+            <span className="side side1"></span>
+            <span className="side side2"></span>
+            <span className="side side3"></span>
+            <span className="side side4"></span>
+            <span className="shadow"></span>
+          </div>
         </div>
-        
-    )
+      </header>
+
+      <button className="btn start-btn" type="button" onClick={handleClick}>
+        <strong>START</strong>
+        <div id="container-stars">
+          <div id="stars"></div>
+        </div>
+
+        <div id="glow">
+          <div className="circle"></div>
+          <div className="circle"></div>
+        </div>
+      </button>
+    </div>
+  );
+  
 }
