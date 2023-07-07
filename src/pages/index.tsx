b1c1a31a064dc6aac1f09d16c1d96aad5e84edfb
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-07 16:11:23 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/basicos/interface');
+  }, []);
+
   return (
     <div>
       <h1>Principal</h1>
